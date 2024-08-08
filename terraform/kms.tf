d957e6a12ac30195146dca20973ce89696bcde19
Cam Beeler <cameron.beeler@slalom.com>
2024-08-07 22:35:30 -0700

    updating the kms keys

--- terraform/kms.tf
+++ terraform/kms.tf
@@ -3,41 +3,60 @@ resource "aws_kms_key" "s3_bucket_key" {
   deletion_window_in_days = 7
   enable_key_rotation     = true
 
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "Enable IAM User Permissions",
-      "Effect": "Allow",
-      "Principal": {
-        "AWS": "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
-      },
-      "Action": "kms:*",
-      "Resource": "*"
-    },
-    {
-      "Sid": "Allow S3 to use the key",
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "s3.amazonaws.com"
-      },
-      "Action": [
-        "kms:Encrypt",
-        "kms:Decrypt",
-        "kms:ReEncrypt*",
-        "kms:GenerateDataKey*",
-        "kms:DescribeKey"
-      ],
-      "Resource": "*",
-      "Condition": {
-        "StringEquals": {
-          "kms:ViaService": "s3.${var.aws_region}.amazonaws.com",
-          "kms:CallerAccount": "${data.aws_caller_identity.current.account_id}"
-        }
-      }
-    }
-  ]
+  policy = aws_iam_policy_document.kms_s3_bucket_policy_document.json
+
 }
-EOF
+
+resource "aws_iam_policy_document" "kms_s3_bucket_policy_document" {
+  statement {
+    sid    = "AllowS3BucketKeyUsage"
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = [
+        "sns.amazonaws.com",
+        "cloudwatch.amazonaws.com",
+        "lambda.amazonaws.com",
+        "s3.amazonaws.com"
+      ]
+    }
+    actions = [
+      "kms:Encrypt",
+      "kms:Decrypt",
+      "kms:ReEncrypt*",
+      "kms:GenerateDataKey*",
+      "kms:DescribeKey"
+    ]
+    resources = ["*"]
+  }
+
+  # New statement for KMS key maintenance by IAM users in the account
+  statement {
+    sid    = "ManageS3BucketKMSKey"
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = [
+        "arn:aws:iam::${var.aws_account_id}:user/*"
+      ]
+    }
+    actions = [
+      "kms:CancelKeyDeletion", 
+      "kms:Create*", 
+      "kms:Delete*", 
+      "kms:Describe*", 
+      "kms:Disable*", 
+      "kms:Enable*", 
+      "kms:GenerateDataKey", 
+      "kms:Get*", 
+      "kms:List*", 
+      "kms:Put*", 
+      "kms:Revoke*", 
+      "kms:ScheduleKeyDeletion", 
+      "kms:TagResource", 
+      "kms:Update*", 
+      "kms:UntagResource"
+    ]
+    resources = ["*"]
+  }
 }
--- terraform/s3.tf
+++ terraform/s3.tf
@@ -57,3 +57,84 @@ resource "aws_s3_bucket_policy" "attach_s3_bucket_policy" {
   bucket = aws_s3_bucket.cams_s3_bucket.id
   policy = data.aws_iam_policy_document.cams_tf_explore_s3_bucket_policy.json
 }
+
+
+### Create an S3 bucket
+### use cloudposse/s3-bucket/aws module
+### https://registry.terraform.io/modules/cloudposse/s3-bucket/aws/latest
+### create a ingestion key
+### create a processed key
+### create a trigger for a lambda function to process the files in the ingestion key
+module "objects_processing_bucket" {
+  source                  = "cloudposse/s3-bucket/aws"
+  version                 = "4.2.0"
+  name                    = "objects-processing-bucket"
+  versioning_enabled      = false
+  s3_object_ownership     = var.s3_bucket_owner_enforced
+  allow_ssl_requests_only = true
+
+  context = module.this.context
+}
+
+resource "aws_s3_bucket_notification" "bucket-event-notifications" {
+  bucket = module.ods_bucket.bucket_id
+
+  lambda_function {
+    lambda_function_arn = module.trigger-preference-data.arn
+    events              = ["s3:ObjectCreated:Put", "s3:ObjectCreated:CompleteMultipartUpload"]
+    filter_prefix       = "ods-landing/preferences/"
+  }
+
+  lambda_function {
+    lambda_function_arn = module.trigger_ingest_customer_extended_contacts.arn
+    events              = ["s3:ObjectCreated:Put", "s3:ObjectCreated:CompleteMultipartUpload"]
+    filter_prefix       = "ods-landing/customer-contacts/"
+  }
+
+  lambda_function {
+    lambda_function_arn = module.trigger-pre-activation-candidate-psps.arn
+    events              = ["s3:ObjectCreated:Put", "s3:ObjectCreated:CompleteMultipartUpload"]
+    filter_prefix       = "ods-landing/psps-export/"
+  }
+
+  lambda_function {
+    lambda_function_arn = module.trigger-ingest-pre-act-cand-lc.arn
+    events              = ["s3:ObjectCreated:Put", "s3:ObjectCreated:CompleteMultipartUpload"]
+    filter_prefix       = "ods-landing/lc-export/"
+  }
+
+  lambda_function {
+    lambda_function_arn = module.trigger-tribal-lands.arn
+    events              = ["s3:ObjectCreated:Put", "s3:ObjectCreated:CompleteMultipartUpload"]
+    filter_prefix       = "ods-landing/tribal-data/"
+  }
+
+  lambda_function {
+    lambda_function_arn = module.trigger-psps-planned.arn
+    events              = ["s3:ObjectCreated:Put", "s3:ObjectCreated:CompleteMultipartUpload"]
+    filter_prefix       = "ods-landing/psps-planned/"
+  }
+
+  lambda_function {
+    lambda_function_arn = module.trigger-customer-informations.arn
+    events              = ["s3:ObjectCreated:Put", "s3:ObjectCreated:CompleteMultipartUpload"]
+    filter_prefix       = "ods-landing/customerinformations/"
+  }
+
+  lambda_function {
+    lambda_function_arn = module.trigger-child-contacts.arn
+    events              = ["s3:ObjectCreated:Put", "s3:ObjectCreated:CompleteMultipartUpload"]
+    filter_prefix       = "ods-landing/child-contacts/"
+  }
+
+  depends_on = [
+    aws_lambda_permission.trigger-preference-data,
+    aws_lambda_permission.trigger-ingest-pre-act-cand-lc,
+    aws_lambda_permission.trigger-pre-activation-candidate-psps,
+    aws_lambda_permission.trigger_ingest_customer_extended_contacts,
+    aws_lambda_permission.trigger-tribal-lands,
+    aws_lambda_permission.trigger-psps-planned,
+    aws_lambda_permission.trigger-customer-informations,
+    aws_lambda_permission.trigger-child-contacts
+  ]
+}
